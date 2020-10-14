f86d1458be73583d4e8eee56d62ebe03b10bd2c2
Czw996 <459749926@qq.com>
2020-10-14 14:22:32 +0800

    2020.10.14

--- number_precision/number_precision.py
+++ number_precision/number_precision.py
@@ -8,7 +8,6 @@ class NP:
         value = sum(map(lambda num: Decimal(str(num)), args))
         return NP.round(value, ndigits=round)
 
-
     @staticmethod
     def minus(*args, round=None):
         value = sum(map(lambda num: -Decimal(str(num)), args[1:]), Decimal(str(args[0])))
@@ -19,30 +18,13 @@ class NP:
         value = reduce(lambda total, num: total * Decimal(str(num)), [Decimal(1), *args])
         return NP.round(value, ndigits=round)
 
-
     @staticmethod
     def divide(*args, round=None):
         value = reduce(lambda total, num: total / Decimal(str(num)), [Decimal(str(args[0])), *args[1:]])
         return NP.round(value, ndigits=round)
 
-
     @staticmethod
     def round(value, ndigits=None):
         if ndigits is not None:
             value = Context(prec=ndigits, rounding=ROUND_HALF_UP).create_decimal(str(value))
-        return float(value)
-
-
-if __name__ == "__main__":
-    results = [
-        0.1 + 0.2,
-        NP.plus(0.1, 0.2),
-        NP.plus(2.3, 2.4),
-        NP.minus(1.0, 0.9),
-        NP.times(3, 0.3),
-        NP.times(0.362, 100),
-        NP.divide(1.21, 1.1),
-        NP.round(0.105, 2),
-    ]
-
-    print(results)
+        return float(value) if '.' in str(value) else int(value)
--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type='text/markdown',
     url='https://github.com/Czw96/NumberPrecision',
-    packages=setuptools.find_packages(exclude=('trash',)),
+    packages=setuptools.find_packages(exclude=['trash']),
     classifiers=[
         'Programming Language :: Python :: 3',
         'License :: OSI Approved :: MIT License',
